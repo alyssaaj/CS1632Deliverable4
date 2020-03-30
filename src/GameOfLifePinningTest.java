6717fbb5f19d10870e809bae4f706605cf0c27ff
Alyssa Jordan <alj81@pitt.edu>
2020-03-30 16:24:45 -0400

    comments

--- src/GameOfLifePinningTest.java
+++ src/GameOfLifePinningTest.java
@@ -120,25 +120,56 @@ public class GameOfLifePinningTest {
 	}
 
 	/* TODO: Write the three pinning unit tests for the three optimized methods */
+
+	/**
+	 * Test case for MainPanel.iterateCell(int x, int y).
+	 * Preconditions: Create a mock Cell array of mocked Cells 
+	 *				  with vertical blinker bar initialized.
+	 * Execution steps: Call mp.iterateCell(2,3).
+	 * Postconditions: Return value is false.
+	 */
 	@Test
 	public void testIterateCellReturnFalse(){
 		// check that an alive cell becomes dead
 		assertFalse("testIterateCellReturnFalse failed!", mp.iterateCell(2,3));
 	}
 
+	/**
+	 * Test case for MainPanel.iterateCell(int x, int y).
+	 * Preconditions: Create a mock Cell array of mocked Cells 
+	 *				  with vertical blinker bar initialized.
+	 * Execution steps: Call mp.iterateCell(3,2).
+	 * Postconditions: Return value is true.
+	 */
 	@Test
 	public void testIterateCellReturnTrue(){
 		// check that a dead cell becomes alive
 		assertTrue("testIterateCellReturnTrue failed!",mp.iterateCell(3,2));
 	}
 
+	/**
+	 * Test case for MainPanel.iterateCell(int x, int y).
+	 * Preconditions: Create a mock Cell array of mocked Cells
+	 *				  with vertical blinker bar initialized.
+	 * Execution steps: Call mp.iterateCell(2,2).
+	 * Postconditions: Return value is true.
+	 */
 	@Test
 	public void testIterateCellMiddle(){
 		// check that the middle alive cell stays alive
 		assertTrue("testIterateCellMiddlefailed!",mp.iterateCell(2,2));
 	}
 
-	
+	/**
+	 * Test case for MainPanel.calculateNextIteration().
+	 * Preconditions: Create a mock Cell array of mocked Cells
+	 *				  with vertical blinker bar initialized.
+	 * Execution steps: Call calculateNextIteration.
+	 * Postconditions: setAlive(true) is called 1 time for the 3 cells turned alive,
+	 *				   setAlive(false) is called 1 time for the 2 cells turned dead,
+	 *				   setAlive(false) is called 20 times for the remaining cells not 
+	 * 				   by the pattern.
+	 */	
 	@Test
 	public void testCalculateNextIteration(){
 		mp.calculateNextIteration();	// horizontal bar created
@@ -165,13 +196,24 @@ public class GameOfLifePinningTest {
 	}
 
 
-
+	/**
+	 * Test case for Cell.toString().
+	 * Preconditions: Create an alive cell.
+	 * Execution steps: Call cell_X.toString().
+	 * Postconditions: Return value is "X".
+	 */
 	@Test
 	public void testBackUpToStringAlive(){
 		// test that an alive cell produces an X when toString is called
 		assertEquals("X", cell_X.toString());
 	}
 
+	/**
+	 * Test case for Cell.toString().
+	 * Preconditions: Create a dead cell.
+	 * Execution steps: Call cell_dot.toString().
+	 * Postconditions: Return value is ".".
+	 */
 	@Test
 	public void testBackUpToStringDead(){
 		// test that a dead cell produces a . when toString is called
